76193765a5195b600368149c08775c173b0cca53
jacksonz <jacksonzarse@zarserarri.att.net>
2014-05-27 18:48:55 -0400

    more query fun

--- javascript/global.js
+++ javascript/global.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 		var linkURL = $( this ).attr( "href" );
 		if( window.location.href.indexOf( linkURL ) > -1) {
 			var a = $(this).parent();
-			window.setTimeout(function(){a.addClass("current");},1000);
+			window.setTimeout(function(){a.animate({marginLeft:"+=10px"}).addClass("current").delay(10).animate({marginLeft:"-=10px"});},1000);
 		}
 	});
 
