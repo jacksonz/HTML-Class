8680ad1814ed76c2f83f4866b6ad256dbf4c0b29
jacksonz <jacksonzarse@zarserarri.att.net>
2014-05-27 18:43:48 -0400

    added delay to nag item selection to make it more obvious

--- javascript/global.js
+++ javascript/global.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
     $( "#menu li a" ).each(function( index ) {
 		var linkURL = $( this ).attr( "href" );
 		if( window.location.href.indexOf( linkURL ) > -1) {
-			$(this).parent().addClass("current")
+			var a = $(this).parent();
+			window.setTimeout(function(){a.addClass("current");},1000);
 		}
 	});
 
