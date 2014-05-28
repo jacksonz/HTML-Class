aa6f537f586463f46e79c6423417c9b1e298a873
jacksonz <jacksonzarse@zarserarri.local>
2014-05-28 09:56:31 -0400

    css tweaks

--- javascript/global.js
+++ javascript/global.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $( "#menu li a" ).each(function( index ) {
 		var linkURL = $( this ).attr( "href" );
 		if( window.location.href.indexOf( linkURL ) > -1) {
-			var a = $(this).parent();
+			var a = $(this).parent().addClass("current2");
 			window.setTimeout(function(){a.animate({marginLeft:"+=10px"}).addClass("current").delay(10).animate({marginLeft:"-=10px"});},1000);
 		}
 	});
